17895c37c6c0623ecff41926883b26adca02813d
LuizTejo <luiz.antoniotejo@gmail.com>
2024-06-06 21:38:21 -0300

    LogicaJS

---
+++ Aula_02/MinhaPrimeiraVariavel/js/resultado.js
@@ -0,0 +1,7 @@
+function resultado(){
+    var nome = "Luiz Antonio";
+    var idade = 35;
+    var curso = "FrontEnd";
+
+    console.log(nome + " " + idade + " " + curso);
+}
---
+++ Aula_03/RetornoFuncao/js/funcao.js
@@ -0,0 +1,10 @@
+function retornanome(nome){
+    var nome = document.getElementById('nome');
+    var resultado = nome.value;
+    alert("Boa noite " + resultado);
+}
+
+function retornanome2(nome){
+    var nome2 = prompt("Informe o seu nome:");
+    alert("Boa noite " + nome2);
+}
